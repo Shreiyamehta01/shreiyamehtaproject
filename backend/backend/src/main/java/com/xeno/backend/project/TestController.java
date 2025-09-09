ba47d833253e496941ec736ee1313581eae13615
shreiya mehta <shreiyamehta01>
2025-09-09 16:30:29 +0530

    Initial commit - Xeno FDE Assignment

---
+++ backend/backend/src/main/java/com/xeno/backend/project/TestController.java
@@ -0,0 +1,13 @@
+package com.xeno.backend.project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot is working!";
+    }
+}
